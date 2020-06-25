aae87796df532864edd3cbd4cd225e730baab895
Iker <ikernoya4@gmail.com>
2020-06-25 14:07:33 -0300

    Created a player prefab

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed;
+    Vector3 movement;
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpeed;
+        transform.position += movement * Time.deltaTime;
+    }
+}
