0bb1831641ef07fae9f2d5c743a4906b4116fe97
Iker <ikernoya4@gmail.com>
2020-06-27 12:10:52 -0300

    Imported fonts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float movementSpeed;
     Vector3 movement;
-
+    Vector3 worldLimit;
+    float shipWidth;
+    float shipHeight;
 
     void Start()
     {
-        
+        shipWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        shipHeight = transform.GetComponent<SpriteRenderer>().bounds.size.z / 2;
     }
     void Update()
     {
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpeed;
+        movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * movementSpeed;
         transform.position += movement * Time.deltaTime;
+
     }
 }
