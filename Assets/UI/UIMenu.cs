fb29180899971cbb05f70fefd8301fb650bda3af
Iker <ikernoya4@gmail.com>
2020-06-27 13:07:27 -0300

    Added new assets and completed Menu functionality

---
+++ Assets/UI/UIMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMenu : MonoBehaviour
+{
+    public void ClickOnCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void ClickOnPlay()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void ClickOnQuit()
+    {
+        Application.Quit();
+    }
+}
