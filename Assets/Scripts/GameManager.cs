ba35446f43c44c4b288e5950160aae757f45dd33
Iker <ikernoya4@gmail.com>
2020-06-28 13:25:52 -0300

    Added icons to represent health

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Text energyText;
-    public Text bombText;
     public Text scoreText;
-    int energy = 100;
+
+    int energy = 3;
     int bombAmmo = 3;
     int score = 0;
+    static GameManager instance = null;
+    public static GameManager Get()
+    {
+        return instance;
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
     private void Start()
     {
-        energyText.text = "Energy: " + energy.ToString();
-        bombText.text = "Bombs: " + bombAmmo.ToString();
         scoreText.text = "Score: " + score.ToString();
+        Player.die += Die;
     }
     void Update()
     {
         
     }
+    void Die()
+    {
+        SceneManager.LoadScene("End");
+        ResetValues();
+    }
+    public void ResetValues()
+    {
+        energy = 3;
+        bombAmmo = 3;
+        score = 0;
+    }
+    private void OnDisable()
+    {
+        Player.die -= Die;
+    }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Animations;
-using UnityEditor.Experimental.GraphView;
+﻿
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float movementSpeed;
-    Vector3 movement;
-    float targetFPS = 60; // Temporary variable
     public GameObject bullet;
+    public GameObject battery1;
+    public GameObject battery2;
+    public GameObject battery3;
+    public delegate void Die();
+    public static event Die die;
+
+    Vector3 movement;
+    public float movementSpeed;
+    float targetFPS = 60; // Temporary variable
     int offsetY = 20;
-    
+    int energy = 3;
 
     void Start()
     {
@@ -22,10 +25,39 @@ public class Player : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         movement = new Vector3(h, v, 0) * movementSpeed * targetFPS;
         transform.position += movement * Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.Joystick1Button0))
         {
             Instantiate(bullet, new Vector3(transform.position.x, transform.position.y + offsetY, 0), Quaternion.identity);
         }
-
+        HealthCondition();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            energy--;
+        }
+        if (energy <= 0)
+        {
+            die();
+        }
+    }
+    void HealthCondition()
+    {
+        if (energy == 3)
+        {
+            battery1.SetActive(true);
+            battery2.SetActive(true);
+            battery3.SetActive(true);
+        }
+        if (energy == 2)
+        {
+            battery1.SetActive(true);
+            battery2.SetActive(true);
+            battery3.SetActive(false);
+        }
+        if (energy == 1)
+        {
+            battery1.SetActive(true);
+            battery2.SetActive(false);
+            battery3.SetActive(false);
+        }
     }
 }
